753f82a51a0cf31f4facf3308d398a587ba13fe3
pritam khergade <pritam.khergade@finarkein.com>
2026-03-02 21:45:44 +0530

    scannre

--- appliance-backend/src/modules/scanner-engine/services/scanner-stats.service.ts
+++ appliance-backend/src/modules/scanner-engine/services/scanner-stats.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import { PrismaService } from "../../shared/prisma.service";
+import { PrismaService } from "../../../shared/prisma.service";
 
 export interface ScannerEngineStats {
   totalRawRules: number;
